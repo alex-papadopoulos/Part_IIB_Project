33a0527bb3347304bf54ac51ea694e7aea069c71
Alex Papadopoulos <alex.papadop99@gmail.com>
2021-11-17 10:34:02 +0000

    Added flags for running and small shape corrections

--- 2D-3D-CNN.py
+++ 2D-3D-CNN.py
@@ -13,6 +13,9 @@ import pickle
 import numpy as np
 import pandas as pd
 
+# Flag for module import
+print("Successfully imported modules")
+
 
 # Parameters
 act = 'relu'
@@ -30,14 +33,21 @@ with open(filename[0], 'rb') as f:
     obj = pickle.load(f)
     uvw3D_field = obj
 
+# Flag for first file
+print("Loaded data from first file")
+
 with open(filename[1], 'rb') as f:
     obj = pickle.load(f)
     uvw3D_field = np.concatenate((uvw3D_field, obj), axis=0)
 
+# Flag for second file
+print("Loaded data from second file")
+
+# Flag for dataset shape
 print(uvw3D_field.shape)
 
 # Define empty 2D dataset
-uvw2D_sec = np.empty([50,256,128,15])
+uvw2D_sec = np.empty([100,256,128,15])
 
 # Ex. 5 cross-sections are used to input of the model
 uvw2D_sec[:,:,:,0:3]=uvw3D_field[:,:,:,15,:]
@@ -48,6 +58,8 @@ uvw2D_sec[:,:,:,12:]=uvw3D_field[:,:,:,143,:]
 
 # The data is divide to training/validation data
 X_train,X_test,y_train,y_test = train_test_split(uvw2D_sec,uvw3D_field,test_size=0.3,random_state=None)
+
+print(X_train.shape)
 
 
 # Input variables
@@ -70,16 +82,23 @@ x_final = Conv3D(3,(3,3,3),activation='linear', padding='same')(x)
 model = Model(input_field,x_final)
 model.compile(optimizer='adam',loss='mse')
 
+# Flag for compiling model
+print("NN Model compiled")
+
 ##################
 # TODO change filepaths
 
 from keras.callbacks import ModelCheckpoint,EarlyStopping
-model_cb=ModelCheckpoint('./Model_cy.hdf5', monitor='val_loss',save_best_only=True,verbose=1)
-early_cb=EarlyStopping(monitor='val_loss', patience=100,verbose=1)
+model_cb = ModelCheckpoint('./Model_cy.hdf5', monitor='val_loss', save_best_only=True, verbose=1)
+early_cb = EarlyStopping(monitor='val_loss', patience=100,verbose=1)
 cb = [model_cb, early_cb]
-history = model.fit(X_train,y_train,epochs=5000,batch_size=100,verbose=1,callbacks=cb,shuffle=True,validation_data=[X_test, y_test])
+# Use reduced epochs for first run - original is 5000
+history = model.fit(X_train,y_train,epochs=500,batch_size=50,verbose=1,callbacks=cb,shuffle=True,validation_data=[X_test, y_test])
 df_results = pd.DataFrame(history.history)
 df_results['epoch'] = history.epoch
 df_results.to_csv(path_or_buf='./Model_cy.csv',index=False)
 
 model.save("./my_model")
+
+# Flag for model.save
+print("Successfully saved trained model")
